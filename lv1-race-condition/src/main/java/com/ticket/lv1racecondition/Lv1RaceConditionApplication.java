cb4e8739eee0c0baab9130bed6228fb6e3424ac4
JaeHyeon Yoo <jhyoo1203@gmail.com>
2025-11-06 21:40:06 +0900

    chore: level 1 미션을 위한 멀티모듈 구성

---
+++ lv1-race-condition/src/main/java/com/ticket/lv1racecondition/Lv1RaceConditionApplication.java
@@ -0,0 +1,13 @@
+package com.ticket.lv1racecondition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lv1RaceConditionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lv1RaceConditionApplication.class, args);
+    }
+
+}
---
+++ lv1-race-condition/src/test/java/com/ticket/lv1racecondition/Lv1RaceConditionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticket.lv1racecondition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lv1RaceConditionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
